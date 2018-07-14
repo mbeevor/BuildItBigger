291323dabe09c7a130bade24043a82ab6382ea69
mbeevor <mbeevor@gmail.com>
2018-07-14 11:29:55 +0100

    Step 3 - local host not found

--- androidLibrary/src/main/java/com/bignerdranch/android/LibraryActivity/LibraryActivity.java
+++ androidLibrary/src/main/java/com/bignerdranch/android/LibraryActivity/LibraryActivity.java
@@ -1,11 +1,11 @@
-package com.bignerdranch.android.jokelibrary;
+package com.bignerdranch.android.LibraryActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import butterknife.ButterKnife;
+import com.bignerdranch.android.jokelibrary.R;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -16,7 +16,6 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-        ButterKnife.bind(this);
 
         jokeTv = findViewById(R.id.joke_tv);
 
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.bignerdranch.android.jokelibrary.LibraryActivity;
-import com.bignerdranch.android.jokeslibrary.JokesClass;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,11 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
 
-        Intent intent = new Intent(this, LibraryActivity.class);
-        JokesClass jokesClass = new JokesClass();
-
-        intent.putExtra("joke", jokesClass.tellJoke());
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(this);
 }
 
 
